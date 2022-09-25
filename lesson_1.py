32745d0f05d7ce2cbbbc6155a7a0727e85b7b970
firuz <gaberud@yandex.ru>
2022-09-25 14:59:35 +0300

    lesson_1 hw-2

--- lesson_1.py
+++ lesson_1.py
@@ -14,3 +14,13 @@ elif week_day > 7:
     print('Это не день недели')
 else:
     print('Сегодня работаем')
+
+
+# 2. Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if not(x or y or z) == (not x and not y and not z):
+                print(x, y, z)
