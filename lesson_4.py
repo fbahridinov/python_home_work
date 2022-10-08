84503339906932e9912b972a5a6527ae7a17d4d6
firuz <gaberud@yandex.ru>
2022-10-08 07:43:20 +0300

    2 задачка

--- lesson_4.py
+++ lesson_4.py
@@ -15,10 +15,46 @@
 
 from decimal import Decimal
 
-real_number = input('Enter a real number: ')
+real_number = Decimal(input('Enter a real number: '))
 
 req_accuracy = input('Enter the required accuracy 0.0001: ')
 
-number = Decimal(real_number)
-number = number.quantize(Decimal(req_accuracy))
+number = real_number.quantize(Decimal(req_accuracy))
 print(number)
+
+# 2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+# Простые делители числа
+#
+# in
+# 54
+#
+# out
+# [2, 3, 3, 3]
+#
+# in
+# 9990
+#
+# out
+# [2, 3, 3, 3, 5, 37]
+#
+# in
+# 650
+#
+# out
+# [2, 5, 5, 13]
+
+
+def numeral(value):
+    l = []
+    prime_number = 2
+    while prime_number <= value:
+        if value % prime_number == 0:
+            l.append(prime_number)
+            value //= prime_number
+            prime_number = 2
+        else:
+            prime_number += 1
+    print(l)
+
+
+numeral(int(input('in: ')))
