784d60ed78ce81f3803e9c9e1a67c7268f73454b
firuz <gaberud@yandex.ru>
2022-10-08 08:09:57 +0300

    3 задачка

--- lesson_4.py
+++ lesson_4.py
@@ -58,3 +58,51 @@ def numeral(value):
 
 
 numeral(int(input('in: ')))
+
+# 3. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности в том же порядке.
+# in
+# 7
+#
+# out
+# [4, 5, 3, 3, 4, 1, 2]
+# [5, 1, 2]
+#
+# in
+# -1
+#
+# out
+# Negative value of the number of numbers!
+# []
+#
+# in
+# 10
+#
+# out
+# [4, 4, 5, 5, 6, 2, 3, 0, 9, 4]
+# [6, 2, 3, 0, 9]
+#
+
+
+import random
+
+your_array = int(input('Arbitrary user number: '))
+
+
+def list_create(number):
+    list_numb = []
+    for index in range(1, number + 1):
+        list_numb.append(random.randint(0, 5))
+
+    print(list_numb)
+
+    result_list = []
+
+    for i in list_numb:
+        if i in result_list:
+            continue
+        else:
+            result_list.append(i)
+    print(result_list)
+
+
+list_create(your_array)
